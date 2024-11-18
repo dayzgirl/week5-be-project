9a09a3f3151c400073da4892ed288d04818016fc
dayzgirl <dayzgirl@gmail.com>
2024-11-17 19:24:58 -0500

    Final commit after verifying everything works

--- src/vergne/daisy/App.java
+++ src/vergne/daisy/App.java
@@ -1,12 +1,18 @@
 package vergne.daisy;
 
+//This will test our logger implementations.
 public class App {
-    public static void main(String[] args) {
-        Logger asteriskLogger = new AsteriskLogger();
+    //Main method
+	public static void main(String[] args) {
+        
+		//Creates instances of both logger types
+		Logger asteriskLogger = new AsteriskLogger();
         Logger spacedLogger = new SpacedLogger();
         
-        asteriskLogger.log("Hello");
+        //Tests asteriskLogger and spacedLogger respectively
+		asteriskLogger.log("Hello");
         asteriskLogger.error("Hello");
+        
         
         spacedLogger.log("Hello");
         spacedLogger.error("Hello");
